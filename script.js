d953905d7c52d017683873c71107cbf36ee1c5ec
TimmyPhamJ <minhthatra@gmail.com>
2023-03-12 14:25:54 +0700

    hi
    Đây là Portfolio của tôi

---
+++ script.js
@@ -0,0 +1,7 @@
+var type = new Typed(".multiple-text",{
+    strings:["Frontend Developer","Data Analyst","Tester Engineer","Studying at FPT Aptech"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
